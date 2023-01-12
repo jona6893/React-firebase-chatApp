ef7e734b66d502e285462bae3b31c15eb9d16ca3
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-01-12 18:11:05 +0100

    Firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,7 +31,7 @@ function Login() {
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">J.Chat</span>
-        <span className="title">Sign Up</span>
+        <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="E-mail" />
           <input type="password" placeholder="Password" />
